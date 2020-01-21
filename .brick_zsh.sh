7fe2d41dc0358cf52242b63193a9dbb055af2c91
= <rkabrick@udel.edu>
2020-01-21 17:22:00 -0500

    Zsh

---
+++ .brick_zsh.sh
@@ -0,0 +1,32 @@
+source /Users/rkabrick/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-mafredri-SLASH-zsh-async/async.plugin.zsh
+fpath+=( /Users/rkabrick/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-mafredri-SLASH-zsh-async )
+source /Users/rkabrick/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-sindresorhus-SLASH-pure/pure.plugin.zsh
+fpath+=( /Users/rkabrick/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-sindresorhus-SLASH-pure )
+source /Users/rkabrick/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
+fpath+=( /Users/rkabrick/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions )
+source /Users/rkabrick/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions/zsh-completions.plugin.zsh
+fpath+=( /Users/rkabrick/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions )
+source /Users/rkabrick/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-rapgenic-SLASH-zsh-git-complete-urls/zsh-git-complete-urls.plugin.zsh
+fpath+=( /Users/rkabrick/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-rapgenic-SLASH-zsh-git-complete-urls )
+source /Users/rkabrick/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zpm-zsh-SLASH-ssh/ssh.plugin.zsh
+fpath+=( /Users/rkabrick/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zpm-zsh-SLASH-ssh )
+source /Users/rkabrick/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-hcgraf-SLASH-zsh-sudo/sudo.plugin.zsh
+fpath+=( /Users/rkabrick/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-hcgraf-SLASH-zsh-sudo )
+source /Users/rkabrick/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-supercrabtree-SLASH-k/k.plugin.zsh
+fpath+=( /Users/rkabrick/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-supercrabtree-SLASH-k )
+
+source /Users/rkabrick/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zpm-zsh-SLASH-dropbox/dropbox.plugin.zsh
+fpath+=( /Users/rkabrick/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zpm-zsh-SLASH-dropbox )
+source /Users/rkabrick/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-laggardkernel-SLASH-zsh-thefuck/zsh-thefuck.plugin.zsh
+fpath+=( /Users/rkabrick/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-laggardkernel-SLASH-zsh-thefuck )
+source /Users/rkabrick/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/pip/pip.plugin.zsh
+fpath+=( /Users/rkabrick/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/pip )
+source /Users/rkabrick/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/git/git.plugin.zsh
+fpath+=( /Users/rkabrick/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/git )
+source /Users/rkabrick/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/command-not-found/command-not-found.plugin.zsh
+fpath+=( /Users/rkabrick/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/command-not-found )
+
+source /Users/rkabrick/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/vi-mode/vi-mode.plugin.zsh
+fpath+=( /Users/rkabrick/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/vi-mode )
+source /Users/rkabrick/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-history-substring-search/zsh-history-substring-search.plugin.zsh
+fpath+=( /Users/rkabrick/Library/Caches/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-history-substring-search )
